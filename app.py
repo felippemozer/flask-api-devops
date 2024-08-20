1d7577b2f4279260bae8c18d5e005219d7d4d514
felippemozer <felippemozer22@gmail.com>
2024-08-20 20:54:50 -0300

    feat: create user model

--- app.py
+++ app.py
@@ -1,8 +1,27 @@
 from flask import Flask
 from flask_restful import Resource, Api
+from flask_mongoengine import MongoEngine
 
 app = Flask(__name__)
 api = Api(app)
+db = MongoEngine(app)
+
+
+app.config["MONGODB_SETTINGS"] = {
+    "db": "users",
+    "host": "mongodb",
+    "port": 27017,
+    "username": "admin",
+    "password": "admin"
+}
+
+
+class UserModel(db.Document):
+    cpf = db.StringField(required=True, unique=True)
+    email = db.EmailField(required=True)
+    first_name = db.StringField(required=True)
+    last_name = db.StringField(required=True)
+    birth_date = db.DateTimeField(required=True)
 
 
 class Users(Resource):
