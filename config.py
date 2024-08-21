2b3d20795861c4370721891add90225834ada498
felippemozer <felippemozer22@gmail.com>
2024-08-20 22:42:14 -0300

    refactor: pass config data from envs

--- config.py
+++ config.py
@@ -1,7 +1,10 @@
+import os
+
+
 class DevConfig():
     MONGODB_SETTINGS = {
-        "db": "users",
-        "host": "mongodb",
-        "username": "admin",
-        "password": "admin"
+        "db": os.getenv("MONGO_DB"),
+        "host": os.getenv("MONGO_HOST"),
+        "username": os.getenv("MONGO_USERNAME"),
+        "password": os.getenv("MONGO_PASSWORD")
     }
