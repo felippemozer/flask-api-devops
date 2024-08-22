463e19712437ae989b41f86ba5658dcf89ea347c
felippemozer <felippemozer22@gmail.com>
2024-08-22 19:19:32 -0300

    feat: create production config

--- config.py
+++ config.py
@@ -1,17 +1,26 @@
 import os
 
 
+MONGO_DB = "users"
+
+
 class DevConfig():
     MONGODB_SETTINGS = {
-        "db": os.getenv("MONGO_DB"),
+        "db": MONGO_DB,
         "host": os.getenv("MONGO_HOST"),
         "username": os.getenv("MONGO_USERNAME"),
         "password": os.getenv("MONGO_PASSWORD")
     }
 
 
+class ProdConfig():
+    MONGODB_SETTINGS = {
+        "host": os.getenv("MONGO_HOST"),
+    }
+
+
 class MockConfig():
     MONGODB_SETTINGS = {
-        "db": "users",
+        "db": MONGO_DB,
         "host": "mongomock://localhost",
     }
